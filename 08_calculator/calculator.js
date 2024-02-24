2554e6026b4409a49bcf6d24c1de1089b3b0ccf6
Mina <minaschool11@gmail.com>
2024-02-24 18:11:34 +0200

    Solved: 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length === 0)
+    return 0;
+  else if(arr.length == 1)
+    return arr[0];
+  else {
+    return arr.reduce((sum, n) => sum + n);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, n) => sum * n);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0)
+    return 1;
+  else{
+    let arr = [];
+    for(let i = x; i > 0; i --)
+      arr.push(i);
+    return arr.reduce((sum, n) => sum * n);
+  } 
 };
 
 // Do not edit below this line
