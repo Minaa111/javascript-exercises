4ecc0405cfaee246fee13ac75168df1ca12b0e80
Mina <minaschool11@gmail.com>
2024-02-25 14:44:42 +0200

    Solved: 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ' || str[i] === '!' || str[i] === '.' || str[i] === ',') {
+            str = str.slice(0, i) + str.slice(i + 1);
+            i--; // Decrement i to adjust for removed character
+        }
+    }
 
+
+    const strArr = str.toLowerCase().split('');
+    const revStrArr = str.toLowerCase().split('').reverse();
+    for(let i = 0; i < strArr.length; i++)
+        if(strArr[i] != revStrArr[i])
+            return false;
+    return true;
 };
 
 // Do not edit below this line
